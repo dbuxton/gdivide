30d13d06333fded471172df13b210442fb9f3e41
David Buxton <david@arachnys.com>
2016-08-29 14:53:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# GDivide
+
+A simple command-line tool to divide your personal GMail from your work
+Google Apps/GMail account. Remove personal messages from your work accounts while still keeping them available in the cloud.
+
+Uses the [GMail API](https://developers.google.com/gmail/api/v1/reference).
+
+## Why use
+
+You have a work email on Google Apps and a personal GMail (or Google Apps)
+account. Your significant other (or mother, or dogsitter) sometimes sends you stuff to
+your work address, which you want to keep, but don't want your
+assistant/auditor/etc to see.
+
+This tool lets you move personal messages that have somehow leaked into your work account back to your personal account. It maintains threads and tries to do some deduplication based on message content.
+
+Essentially all GDivide does is:
+
+*   Search for emails **to** or **from** your `private-correspondents` in your **work** email
+*   Check each message to see whether you already have it in your **private** email
+    *   If not, copy them and delete original
+    *   If so, just delete the original
+
+### Why not use [Thunderbird, Outlook, another IMAP client] instead?
+
+Yes, with a bit of patience you can get the same result from using an IMAP client to transfer messages. However things like correct date display on imported messages can be a crapshoot, and processing large numbers of messages in Thunderbird or Outlook is no fun.
+
+## Limitations
+
+*   Only works with GMail accounts
+*   May result in duplicates (if e.g. some messages were copied to, or forwarded from, your personal account). Tries to do deduplication using [simhash](https://github.com/leonsim/simhash) but YMMV. You can disable deduplication with the `--skip-deduplicate` option.
+*   Very wasteful in terms of requests - does several requests for each message rather than [batch API](https://developers.google.com/gmail/api/guides/batch)
+*   No error handling to speak of
+*   Only filtering by sender/addressee email address is supported. If you want to erase all mentions of a person from your email, you will have to send a PR ;)
+*   Does not alter backups or [Google Vault](https://www.google.com/work/apps/business/products/vault/) audit history of your email
+*   Not sure how/if it deals with chats
+
+## Lack of warranty
+
+This tool will make changes to BOTH email accounts that you give it. It may result in you losing data (although it doesn't delete anything directly, just moves to trash, so you should be able to undo any changes if you need to as long as you move quick-ish). You should make sure you have backups of any important data before using it. If you want to get a full dump of your email data you can do so via [Google Takeout](https://www.google.com/settings/takeout).
+
+I take no responsibility for any damage resulting from use of this tool, howsoever caused, including by negligence.
+
+### Inclusion of client secret in this repo
+
+Based on https://developers.google.com/identity/protocols/OAuth2InstalledApp - in particular
+
+> Installed apps are distributed to individual machines, and it is assumed that these apps cannot keep secrets.
+
+I am distributing this app with oauth secret available in the code. I may revoke this at any time for any or no reason if there is any abuse associated with it.
+
+## Usage
+
+1.  Clone repo
+2.  `pip install -r requirements.txt`
+3.  `python gdivide.py --work-gmail me@work.com --home-gmail me@personal.com --private-correspondents girlfriend@example.com mum@gmail.com`
+4.  Sit back and wait. Possibly a really long time depending on number of messages and their size. (GDivide copies attachments too).
+5.  Empty work email trash to permanently remove transferred email. BE CAREFUL!
+
+### Options
+
+    --work-gmail [name@example.com]
+        The account you want to move emails FROM.
+        WARNING: EMAILS IN THIS ACCOUNT MAY GET TRASHED/DELETED
+    --home-gmail [name@example.com]
+        The account you want to move emails TO.
+        WARNING: EMAILS WILL BE ADDED TO THIS ACCOUNT
+    --private-correspondents PRIVATE_CORRESPONDENTS [PRIVATE_CORRESPONDENTS ...]
+        Email address(es) that you want to cleanse from your
+        work google account and move to home gmail
+    --limit [INT]
+        Only process this many messages
+    --dry-run
+        Don't actually make any changes to either home or work accounts
+    --skip-deduplicate
+        Don't attempt to deduplicate based on existing emails in target inbox
+    --clear-credentials
+        Delete any stored OAuth credentials
+
+## Why I wrote it
+
+IMAP is a pain, and I wanted to see if the GMail API was any better.
+
+The short story is, yes and no. In practice you seem to have to learn about MIME encodings, multipart email structures and a load of other things to do anything remotely useful.
+
+However if you want to do simple things, it's a lot easier to understand than IMAP.
+
+## License
+
+Copyright (c) 2016 David Buxton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ gdivide.py
@@ -0,0 +1,386 @@
+import os
+import httplib2
+import oauth2client
+import argparse
+import sys
+import shutil
+import email
+import base64
+import simhash
+import io
+import googleapiclient
+
+from email.header import decode_header
+from datetime import timedelta, datetime
+from dateutil.parser import parse
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+
+parser = argparse.ArgumentParser(parents=[tools.argparser])
+parser.add_argument(
+    '--private-correspondents',
+    help="Email address(es) that you want to cleanse from your work address and move to home address",
+    nargs='+',
+)
+parser.add_argument(
+    '--work-gmail',
+    help="Work gmail address (to move messages *from*)",
+)
+parser.add_argument(
+    '--home-gmail',
+    help="Home gmail address (to move messages *to*)",
+)
+parser.add_argument(
+    '--dry-run',
+    action="store_true",
+    help="Don't make any modifications",
+)
+parser.add_argument(
+    '--clear-credentials',
+    action='store_true',
+    help='Delete saved OAuth credentials (will wipe entire ~/.gdivide-credentials directory)',
+)
+parser.add_argument(
+    '--skip-deduplicate',
+    action='store_true',
+    help='Skip duplicate detection - may create duplicates',
+)
+parser.add_argument(
+    '--limit',
+    type=int,
+    default=None,
+    help='Limit to processing this number of messages',
+)
+
+flags = parser.parse_args()
+
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'GDivide'
+DIRECTIONS = ['to', 'from']
+LABEL_NAME = 'gdivide'
+SIMHASH_DISTANCE = 3
+
+
+def _credential_dir():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.gdivide-credentials')
+    return credential_dir
+
+
+def get_credentials(email, email_type="Unknown"):
+    """
+    Get and locally cache OAuth credentials.
+    """
+    if not os.path.exists(_credential_dir()):
+        os.makedirs(_credential_dir())
+    credential_path = os.path.join(_credential_dir(), "%s.json" % email)
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        print("************************************************************************")
+        print("Please authorize for your %s (%s) gmail account." % (email, email_type))
+        print("************************************************************************")
+        flow = client.flow_from_clientsecrets(
+            filename=CLIENT_SECRET_FILE,
+            scope=SCOPES,
+            login_hint=email,
+        )
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, flags)
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('gmail', 'v1', http=http)
+        resp = service.users().getProfile(userId='me').execute()
+        if not resp['emailAddress'] == email:
+            clear_credentials()
+            print("ERROR: email address does not match credentials.")
+            print("Stored credentials cleared - you will need to re-authenticate.")
+            sys.exit(1)
+    return credentials
+
+
+def clear_credentials():
+    if os.path.exists(_credential_dir()):
+        shutil.rmtree(_credential_dir(), ignore_errors=True)
+    else:
+        print("No saved credentials found")
+    return sys.exit(0)
+
+
+class Divider:
+
+    def __init__(self, home_credentials, work_credentials,
+            private_correspondents, limit=None, skip_deduplicate=False,
+            dry_run=False):
+        self.private_correspondents = private_correspondents
+        home_http = home_credentials.authorize(httplib2.Http())
+        work_http = work_credentials.authorize(httplib2.Http())
+        self.home_service = discovery.build('gmail', 'v1', http=home_http)
+        self.work_service = discovery.build('gmail', 'v1', http=work_http)
+        self.limit = limit
+        self.skip_deduplicate = skip_deduplicate
+        self._label = None
+        self.dry_run = dry_run
+        self.thread_map = {}
+        self.stats_inserted = 0
+        self.stats_trashed = 0
+
+    def run(self):
+        """Entry point
+        """
+        resp = self.get_private_messages_from_work()
+        messages = resp['messages']
+        if self.limit is not None:
+            messages = messages[:self.limit]
+        for message_id in messages:
+            if self.dry_run:
+                print("Moving message with id %s (dry run - no changes will be made!)" % message_id)
+            else:
+                print("Moving message with id %s" % message_id)
+            # Obeys `dry_run`
+            self.move_message(message_id)
+        print('Finished - created %s messages, trashed %s' % (self.stats_inserted, self.stats_trashed))
+
+    def get_or_create_label(self):
+        """Ensures that uploaded messages have the gdivide label set
+        """
+        if self._label:
+            return self._label
+        resp = self.home_service.users().labels().list(userId='me').execute()
+        for label in resp['labels']:
+            if label['name'] == LABEL_NAME:
+                self._label = label
+                return self._label
+        if self.dry_run:
+            print("Would have created label %s" % LABEL_NAME)
+        else:
+            label = self.home_service.users().labels().create(userId='me',
+                body={
+                    'messageListVisibility': 'hide',
+                    'name': LABEL_NAME,
+                    'labelListVisibility': 'labelHide',
+                }).execute()
+            print("Created label with id %s" % label['id'])
+            self._label = label
+        return self._label
+
+    def _get_messages_page(self, service, query, page_token=None):
+        print("Getting results page for query %s" % query)
+        return service.users().messages().list(
+            userId='me',
+            q=query,
+            pageToken=page_token,
+        ).execute()
+
+    def _get_messages(self, service, query):
+        all_messages = []
+        resp = self._get_messages_page(service, query)
+        if 'messages' in resp:
+            all_messages.extend(resp['messages'])
+        while 'nextPageToken' in resp:
+            resp = self._get_messages_page(self.work_service, query,
+                resp['nextPageToken'])
+            all_messages.extend(resp['messages'])
+        return all_messages
+
+    def get_private_messages_from_work(self):
+        """Gets a list of message ids matching private_correspondents
+
+        Returns: dict with two keys, `threads` and `messages`
+        """
+        all_messages = []
+        for correspondent in self.private_correspondents:
+            for direction in DIRECTIONS:
+                query = '%s:%s' % (direction, correspondent)
+                all_messages.extend(self._get_messages(self.work_service, query))
+        all_threads = [i['threadId'] for i in all_messages]
+        all_threads = list(set(all_threads))
+        all_messages = [i['id'] for i in all_messages]
+        all_messages = list(set(all_messages))
+        print("Found %s messages (%s threads) sent %s %s" % (
+            len(all_messages),
+            len(all_threads),
+            '/'.join(DIRECTIONS),
+            ', '.join(self.private_correspondents),
+        ))
+        return {
+          'threads': all_threads,
+          'messages': all_messages,
+        }
+
+    def move_message(self, message_id):
+        """Moves a message FROM work TO home email.
+
+        Unless --skip-deduplicate is set, this will check for duplicates by:
+        """
+        message = self.get_raw_message(self.work_service, message_id)
+        duplicate = None
+        # If not a duplicate or checking turned off, put message and
+        # delete original
+        if not self.skip_deduplicate:
+            duplicate = self.check_duplicate(message)
+        if self.skip_deduplicate or not duplicate:
+
+            if message['threadId'] in self.thread_map:
+                thread_id = self.thread_map[message['threadId']]
+            else:
+                thread_id = None
+
+            if self.dry_run:
+                print("Would have inserted message id %s: (size %d) %s" % (message_id,
+                    len(message['raw']), message['snippet'],))
+            else:
+                print("Inserting message id %s: %s" % (message_id,
+                    message['snippet']))
+                attachments = []
+                raw = message['raw']
+                body = {
+                    'labelIds': [self.get_or_create_label()['id']],
+                }
+                if thread_id:
+                    body['threadId'] = thread_id
+                b = io.BytesIO()
+                message_bytes = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+                b.write(message_bytes)
+                media_body = googleapiclient.http.MediaIoBaseUpload(b, mimetype='message/rfc822')
+                resp = self.home_service.users().messages().insert(
+                    userId='me',
+                    internalDateSource='dateHeader',
+                    body=body,
+                    media_body=media_body,
+                ).execute()
+                self.stats_inserted += 1
+                # Add mapping between old and new threads
+                self.thread_map[message['threadId']] = resp['threadId']
+            if self.dry_run:
+                print("Would have trashed original message %s" % (message_id,))
+            else:
+                print("Trashing original message %s" % (message_id,))
+                resp = self.trash_message(message_id)
+                self.stats_trashed += 1
+        else:
+            print("Skipping message id %s: found duplicate %s in target" % (
+                message_id, duplicate))
+            if self.dry_run:
+                print("Would have trashed original message %s" % (message_id,))
+            else:
+                print("Trashing original message %s" % (message_id,))
+                resp = self.trash_message(message_id)
+                self.stats_trashed += 1
+
+    def trash_message(self, message_id):
+        """Moves original message to trash"""
+        return self.work_service.users().messages().trash(userId='me',
+            id=message_id).execute()
+
+    def get_raw_message(self, service, message_id):
+        """
+        Gets the message from selected account.
+
+        Encoded raw form is available as `raw` key of returned dict
+        """
+        message = service.users().messages().get(userId='me',
+            id=message_id, format='raw').execute()
+        msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+        mime_msg = email.message_from_string(msg_str)
+        message['decoded'] = mime_msg
+        return message
+
+    def check_duplicate(self, message):
+        """Checks to see if `message` already exists in home gmail.
+
+        There's no easy way of doing this because GMail doesn't allow you
+        to search for a message hash.
+
+        Instead, we:
+
+        *   Search for the same subject with the same datestamp
+        *   Download any messages that match
+        *   Compare the raw message bodies against each other
+        *   Return True if they match
+
+        Returns: id of duplicate message if message already exists, None otherwise
+        """
+        subject = self._get_subject(message)
+        date = self._get_date(message)
+        from_date = date - timedelta(days=1)
+        to_date = date + timedelta(days=1)
+        query = "subject:\"%s\" after:%s before:%s" % (
+            subject,
+            from_date.strftime('%Y/%m/%d'),
+            to_date.strftime('%Y/%m/%d'),
+        )
+        similar_messages = self._get_messages(self.home_service, query=query)
+        if similar_messages:
+            for pd in similar_messages:
+                potential_duplicate = self.get_raw_message(self.home_service, pd['id'])
+                if self._check_message_duplicate(message, potential_duplicate):
+                    return potential_duplicate['id']
+        return None
+
+    def _check_message_duplicate(self, message1, message2):
+        """Checks messages.
+
+        Currently just compares first part of payload content but in future will want to
+        compare all payloads as the first bit may vary for legitimate reasons.
+        """
+        if message1['raw'] == message2['raw']:
+            return True
+        email1 = email.message_from_string(base64.urlsafe_b64decode(message1['raw'].encode('ASCII')))
+        email2 = email.message_from_string(base64.urlsafe_b64decode(message2['raw'].encode('ASCII')))
+        if (not email1.is_multipart()) and (not email2.is_multipart()):
+            if email1.get_payload() == email2.get_payload():
+                return True
+            else:
+                return False
+        elif email1.is_multipart() and email2.is_multipart():
+            if len(email1.get_payload()) != len(email2.get_payload()):
+                return False
+            charset1 = email1.get_payload(0).get_content_charset() or 'utf-8'
+            charset2 = email2.get_payload(0).get_content_charset() or 'utf-8'
+            payload1 = str(email1.get_payload(0)).decode(charset1)
+            payload2 = str(email2.get_payload(0)).decode(charset2)
+            distance = simhash.Simhash(payload1).distance(simhash.Simhash(payload2))
+            print("Similarity distance between messages is %s" % distance)
+            if distance < SIMHASH_DISTANCE: # MAGIC NUMBER - no idea what this should be
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def _get_subject(self, message):
+        for h, v in message['decoded'].items():
+            if h.lower() == 'subject':
+                return v
+        return ""
+
+    def _get_date(self, message):
+        """This is a timestamp
+        """
+        return datetime.fromtimestamp(float(message['internalDate'])/1000)
+
+
+def main():
+    if flags.clear_credentials:
+        clear_credentials()
+        print('Credentials cleared!')
+    else:
+        client = Divider(
+            home_credentials=get_credentials(email=flags.home_gmail,
+                email_type='home'),
+            work_credentials=get_credentials(email=flags.work_gmail,
+                email_type='work'),
+            private_correspondents=flags.private_correspondents,
+            limit=flags.limit,
+            skip_deduplicate=flags.skip_deduplicate,
+            dry_run=flags.dry_run,
+        )
+        client.run()
+
+
+if __name__ == '__main__':
+    main()
